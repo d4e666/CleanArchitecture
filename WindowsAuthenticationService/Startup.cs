aa33af2067538f15b686ca3ea02ddd95592af106
Wesley Kenis <keniswe@aarixa.be>
2019-06-12 17:26:13 +0200

    Architecture: Add swagger to Authentication services

--- WindowsAuthenticationService/Startup.cs
+++ WindowsAuthenticationService/Startup.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace WindowsAuthenticationService
 {
@@ -17,7 +11,7 @@ namespace WindowsAuthenticationService
     {
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            this.Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
@@ -25,7 +19,22 @@ namespace WindowsAuthenticationService
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(
+                options =>
+                {
+                    options.SwaggerDoc(
+                        "v1.0",
+                        new Info
+                        {
+                            Version = "v1.0",
+                            Title = "BackOffice",
+                            Description = "v1 API Description",
+                            TermsOfService = "Terms of usage v1"
+                        });
+                });
+            services.AddMvc()
+                    .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,6 +49,17 @@ namespace WindowsAuthenticationService
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(
+                configuration =>
+                {
+                    configuration.SwaggerEndpoint("/swagger/v1.0/swagger.json", "v1.0");
+                });
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
 
             app.UseHttpsRedirection();
             app.UseMvc();
